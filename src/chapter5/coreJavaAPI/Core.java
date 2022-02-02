37c73f47347c05ea8f3976ce5bbe5ef21b70e1f9
Lucas de Castro <lucasdec88@gmail.com>
2022-02-02 13:25:28 +0000

    Core Java API

--- src/chapter5/coreJavaAPI/Core.java
+++ src/chapter5/coreJavaAPI/Core.java
@@ -1,10 +1,12 @@
 package chapter5.coreJavaAPI;
 
+import java.util.Arrays;
+
 public class Core {
 
 	
-	public static void main(String[] args) {
-		
+	public static void main(String[]  args) {
+		arrayCreation();
 	}
 	
 	static public void arrayCreation() {
@@ -18,5 +20,18 @@ public class Core {
 		
 		int[] x,y;//2 arrays
 		int z[],u;//z array, u is a normal int
+		
+		String stringArray [] = {"L"};
+		Object[] o = stringArray;
+//		o[0] = new StringBuilder();//compile-time error
+//		System.out.println(o[0]);//run-time ArrayStoreException cuz it's orinally String[]
+		
+		System.out.println("Mismatch method");
+		int mismatch = Arrays.mismatch(stringArray, new String[]{"L","C"});
+		System.out.println("mismatch at position: " + mismatch);
+		
+		int parseInt = Integer.parseInt("2");//parseInt() returns primitive
+		Integer valueOf = Integer.valueOf("2");// valueof() returns wrapper class
+	
 	}
 }
