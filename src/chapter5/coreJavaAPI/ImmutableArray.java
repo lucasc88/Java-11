85ff9f09bbe3be4c46055e87aa16beef549a2c7a
Lucas de Castro <lucasdec88@gmail.com>
2022-02-03 09:03:10 +0000

    Immutable List

---
+++ src/chapter5/coreJavaAPI/ImmutableArray.java
@@ -0,0 +1,34 @@
+package chapter5.coreJavaAPI;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ImmutableArray {
+
+	private static String [] stringArray = {"Lucas", "Jhon"};
+	
+	public static void main(String...args) {
+		List<String> list = Arrays.asList(stringArray);//it's just a copy
+		
+		List<String> listOf = List.of(stringArray);//Immutable List
+		
+		System.out.println("list size (copy from array): " + list.size());
+		System.out.println("listOf size (copy from array - Immutable List): " + list.size());
+		
+		//add or remove will throw UnsupportedOperationException because is changing the original array size, but set is possible
+		list.set(1, "Lucas again");
+//		list.add("Lucas UnsupportedOperationException");
+//		listOf.set(0, "Lucas trying set in Immutable List: UnsupportedOperationException");//Immutable List
+		
+		System.out.print("stringArray: ");
+		for (String string : stringArray) {
+			System.out.print(" " + string);
+		}
+		System.out.println();
+		
+		System.out.println(list);
+		
+		
+	}
+}
